b67cbb3065ab2f07ff2667535f21a96ab2107d91
pane2004 <alexlu2000clash@gmail.com>
2023-10-19 23:45:27 -0400

    format connect state

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { QRCodeSVG } from "qrcode.react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { useDisconnect, useSwitchNetwork } from "wagmi";
 import {
   ArrowLeftOnRectangleIcon,
-  ArrowTopRightOnSquareIcon,
   ArrowsRightLeftIcon,
   CheckCircleIcon,
   ChevronDownIcon,
   ChevronUpIcon,
   DocumentDuplicateIcon,
-  QrCodeIcon,
 } from "@heroicons/react/24/outline";
 import { Address, Balance, BlockieAvatar } from "~~/components/scaffold-eth";
 import { useAutoConnect, useNetworkColor } from "~~/hooks/scaffold-eth";
-import { getBlockExplorerAddressLink, getTargetNetwork } from "~~/utils/scaffold-eth";
+import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 /**
  * Custom Wagmi Connect Button (watch balance + custom design)
@@ -33,9 +30,6 @@ export const RainbowKitCustomConnectButton = () => {
     <ConnectButton.Custom>
       {({ account, chain, openConnectModal, mounted }) => {
         const connected = mounted && account && chain;
-        const blockExplorerAddressLink = account
-          ? getBlockExplorerAddressLink(getTargetNetwork(), account.address)
-          : undefined;
 
         return (
           <>
@@ -112,107 +106,65 @@ export const RainbowKitCustomConnectButton = () => {
               }
 
               return (
-                <div className="px-2 flex justify-end items-center">
-                  <div className="flex flex-col items-center mr-1">
-                    <Balance address={account.address} className="min-h-0 h-auto" />
-                    <span className="text-xs" style={{ color: networkColor }}>
-                      {chain.name}
-                    </span>
-                  </div>
-                  <div className="dropdown dropdown-end leading-3">
-                    <label
-                      tabIndex={0}
-                      className="btn btn-secondary btn-sm pl-0 pr-2 shadow-md dropdown-toggle gap-0 !h-auto"
-                    >
-                      <BlockieAvatar address={account.address} size={30} ensImage={account.ensAvatar} />
-                      <span className="ml-2 mr-1">{account.displayName}</span>
-                      <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
-                    </label>
+                <div className="px-2 flex flex-col justify-end items-center space-y-4">
+                  <div>
                     {toggle && (
-                      <ul
-                        tabIndex={0}
-                        className="dropdown-content menu z-[2] p-2 mt-2 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
-                      >
+                      <ul className="p-2 mt-2 bg-sky-700 rounded-md gap-1">
                         <li>
                           {addressCopied ? (
-                            <div className="btn-sm !rounded-xl flex gap-3 py-3">
-                              <CheckCircleIcon
-                                className="text-xl font-normal h-6 w-4 cursor-pointer ml-2 sm:ml-0"
-                                aria-hidden="true"
-                              />
-                              <span className=" whitespace-nowrap">Copy address</span>
+                            <div className="flex gap-3 py-3">
+                              <CheckCircleIcon className="text-xl h-6 w-4 ml-2" aria-hidden="true" />
+                              <span>Copy address</span>
                             </div>
                           ) : (
-                            <CopyToClipboard
-                              text={account.address}
-                              onCopy={() => {
-                                setAddressCopied(true);
-                                setTimeout(() => {
-                                  setAddressCopied(false);
-                                }, 800);
-                              }}
-                            >
-                              <div className="btn-sm !rounded-xl flex gap-3 py-3">
-                                <DocumentDuplicateIcon
-                                  className="text-xl font-normal h-6 w-4 cursor-pointer ml-2 sm:ml-0"
-                                  aria-hidden="true"
-                                />
-                                <span className=" whitespace-nowrap">Copy address</span>
-                              </div>
-                            </CopyToClipboard>
+                            <button type="button">
+                              <CopyToClipboard
+                                text={account.address}
+                                onCopy={() => {
+                                  setAddressCopied(true);
+                                  setTimeout(() => {
+                                    setAddressCopied(false);
+                                  }, 800);
+                                }}
+                              >
+                                <div className="flex gap-3 py-3">
+                                  <DocumentDuplicateIcon className="text-xl h-6 w-4 ml-2" aria-hidden="true" />
+                                  <span>Copy address</span>
+                                </div>
+                              </CopyToClipboard>
+                            </button>
                           )}
                         </li>
                         <li>
-                          <label htmlFor="qrcode-modal" className="btn-sm !rounded-xl flex gap-3 py-3">
-                            <QrCodeIcon className="h-6 w-4 ml-2 sm:ml-0" />
-                            <span className="whitespace-nowrap">View QR Code</span>
-                          </label>
-                        </li>
-                        <li>
-                          <button className="menu-item btn-sm !rounded-xl flex gap-3 py-3" type="button">
-                            <ArrowTopRightOnSquareIcon className="h-6 w-4 ml-2 sm:ml-0" />
-                            <a
-                              target="_blank"
-                              href={blockExplorerAddressLink}
-                              rel="noopener noreferrer"
-                              className="whitespace-nowrap"
-                            >
-                              View on Block Explorer
-                            </a>
-                          </button>
-                        </li>
-                        <li>
-                          <button
-                            className="menu-item text-error btn-sm !rounded-xl flex gap-3 py-3"
-                            type="button"
-                            onClick={() => disconnect()}
-                          >
-                            <ArrowLeftOnRectangleIcon className="h-6 w-4 ml-2 sm:ml-0" /> <span>Disconnect</span>
+                          <button className="flex gap-3 py-3 text-error" type="button" onClick={() => disconnect()}>
+                            <ArrowLeftOnRectangleIcon className="h-6 w-4 ml-2" />
+                            <span>Disconnect</span>
                           </button>
                         </li>
                       </ul>
                     )}
+                    <button
+                      tabIndex={0}
+                      className="flex flex-row bg-sky-500 rounded-md px-12 py-5"
+                      type="button"
+                      onClick={() => {
+                        setToggle(ex => !ex);
+                      }}
+                    >
+                      <BlockieAvatar address={account.address} size={30} ensImage={account.ensAvatar} />
+                      <span className="ml-2 mr-1">{account.displayName}</span>
+                      {toggle ? (
+                        <ChevronUpIcon className="h-6 w-4 ml-2" />
+                      ) : (
+                        <ChevronDownIcon className="h-6 w-4 ml-2" />
+                      )}
+                    </button>
                   </div>
-                  <div>
-                    <input type="checkbox" id="qrcode-modal" className="modal-toggle" />
-                    <label htmlFor="qrcode-modal" className="modal cursor-pointer">
-                      <label className="modal-box relative">
-                        {/* dummy input to capture event onclick on modal box */}
-                        <input className="h-0 w-0 absolute top-0 left-0" />
-                        <label
-                          htmlFor="qrcode-modal"
-                          className="btn btn-ghost btn-sm btn-circle absolute right-3 top-3"
-                        >
-                          ✕
-                        </label>
-                        <div className="space-y-3 py-6">
-                          <div className="flex space-x-4 flex-col items-center gap-6">
-                            <QRCodeSVG value={account.address} size={256} />
-                            <Address address={account.address} format="long" disableAddressLink />
-                          </div>
-                        </div>
-                      </label>
-                    </label>
+                  <div className="flex flex-col items-center mr-1">
+                    <Balance address={account.address} />
+                    <span className="text-xs" style={{ color: networkColor }}>
+                      {chain.name}
+                    </span>
                   </div>
                 </div>
               );
