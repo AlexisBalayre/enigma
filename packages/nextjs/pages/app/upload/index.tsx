896950084f3cf20daedd74671255dc6fcd1b95af
Bobliuuu <mmmzzz66g@gmail.com>
2023-10-22 10:05:59 -0400

    Finished frontend

--- packages/nextjs/pages/app/upload/index.tsx
+++ packages/nextjs/pages/app/upload/index.tsx
@@ -1,24 +1,32 @@
 import { AppNavWrapper } from "~~/components/app/Nav";
-import { useFormik } from 'formik';
+import { Formik } from 'formik';
 import { Button } from '~~/components/UI/button';
-import { Input } from '~~/components/UI/input';
+import Input from '~~/components/UI/input';
 
 export default function Mint() {
-  const formik = useFormik({
+  /*
+  const formik = Formik({
     initialValues: {
-      walletAddress: '',
-      imageURL: '',
-      functionality: '',
+      publicURI: '',
+      image: '',
+      privateURI: ''
     },
     onSubmit: (values: any) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
+  */
+ function handleSubmit() {
+  
+ }
+ function change() {
+
+ }
   return (
     <AppNavWrapper>
       <section className='px-16 pt-24'>
         <h1 className='text-white font-bold text-4xl mb-8'>Mint NFT</h1>
-        <form onSubmit={formik.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <p className='text-white font-medium text-xl mb-4'>
             Enter the image URL.
           </p>
@@ -26,10 +34,9 @@ export default function Mint() {
             id='publicURI'
             name='publicURI'
             type='text'
+            onChange={change}
+            defaultValue='hello'
             placeholder='public URI'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.publicURI}
             classes='mb-12'
           />
           <p className='text-white font-medium text-xl mb-4'>
@@ -39,23 +46,20 @@ export default function Mint() {
             id='image'
             name='image'
             type='file'
+            onChange={change}
             placeholder='public URI'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.image}
-            classes='mb-12'
+            classes='mb-12 text-white'
           />
           <p className='text-white font-medium text-xl mb-4'>
-            Enter the link.
+            Enter the image name.
           </p>
           <Input
             id='privateURI'
             name='privateURI'
             type='text'
+            onChange={change}
+            defaultValue='hello'
             placeholder='private URI'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.privateURI}
             classes='mb-12'
           />
           <Button type='submit' hierarchy='primary'>
